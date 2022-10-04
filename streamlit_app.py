977873d51a77722d71d8f196c543de358fb747da
sarawale <31413913+sarawale@users.noreply.github.com>
2022-10-04 12:38:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents Dinner')
 
    
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Nomega 3 and Blueberry Oatmeal')
-streamlit.text('🥗 kale, Spianch and Smoothie')
-streamlit.text('🐔 Hard Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
 
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
